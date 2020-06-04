8ae489046add262cc64fbe755b9fa0ae5c2a3ea9
gonzalo.guasch <gonzaloguasch@outlook.com>
2020-06-04 18:31:09 -0300

    latin worker template

--- src/LatinWorker.java
+++ src/LatinWorker.java
@@ -1,2 +1,34 @@
+import src.LatinPoisonPill;
+import src.LatinPoisonPillException;
+
 public class LatinWorker extends Thread{
+    private Buffer buffer;
+    private SortedList sortedList;
+    private CountDown countDown;
+
+    public LatinWorker(Buffer buffer){
+        this.buffer = buffer;
+    }
+
+    public LatinWorker(Buffer buffer, SortedList sortedLit, CountDown countDown) {
+        this.buffer = buffer;
+        this.sortedList = sortedList;
+        this.countDown = countDown;
+    }
+
+    public void run(){
+        try{
+            while(true){
+                //Busco un cuadrado latino del buffer
+                //if(this.esCuadradoLatino(cuadradoDeBuffer){ lo agrego a la sorted list}
+                //decremento el countDown
+            }
+        }catch (LatinPoisonPillException latinPoisonPill){
+            System.out.println("Ya no tengo mas cuadrados latinos!");
+        }
+    }
+
+    private boolean esLatinSquare(Object cuadradoLatino){
+     return false;
+    }
 }
--- src/ThreadPool.java
+++ src/ThreadPool.java
@@ -3,12 +3,16 @@ import src.LatinPoisonPill;
 public class ThreadPool {
     private Buffer buffer;
     private Integer amountOfLatinWorkers;
+    private SortedList sortedLit;
+    private CountDown countDown;
 
     public ThreadPool(Buffer buffer, Integer amountOfLatinWorkers) {
+        this.sortedLit = new SortedList();
+        this.countDown = new CountDown(1000); //cantidad de cuadrados latinos
         this.buffer = buffer;
         this.amountOfLatinWorkers = amountOfLatinWorkers;
         for (int i = 0; i < amountOfLatinWorkers; i++) {
-            new LatinWorker().start();
+            new LatinWorker(this.buffer, sortedLit, countDown).start();
         }
 
     }
