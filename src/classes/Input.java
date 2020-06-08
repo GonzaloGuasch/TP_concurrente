ad9cdcf02490a99a434a116662a55a62c113246f
gonzalo.guasch <gonzaloguasch@outlook.com>
2020-06-08 15:34:03 -0300

    minimo refactor

--- src/classes/Input.java
+++ src/classes/Input.java
@@ -5,21 +5,35 @@ import java.util.Scanner;
 public class Input {
 
 
-    public Integer tamBuffer(){
+    public Integer tamBuffer() {
         System.out.print("Tamanio de buffer:");
-        Scanner tamInput = new Scanner(System.in);
-        String tamInString = tamInput.next();
-        Integer tamanio = Integer.parseInt(tamInString);
+        String tamInput = this.scanner().next();
+        try {
+            Integer tamanio = Integer.parseInt(tamInput);
+            return tamanio;
+        } catch (NumberFormatException e) {
+            System.out.println("Tiene que ser un numero el input");
+            System.exit(0);
+        }
 
-        return tamanio;
+        return 0;
     }
 
-    public Integer amountOfLatinWorkers(){
+    public Integer amountOfLatinWorkers() {
         System.out.print("Cantidad de LatinWorkers:");
-        Scanner latinWorkers = new Scanner(System.in);
-        String latinString = latinWorkers.next();
-        Integer latin = Integer.parseInt(latinString);
+        String latinString = this.scanner().next();
+        try {
+            Integer latin = Integer.parseInt(latinString);
+            return latin;
+        } catch (NumberFormatException e) {
+            System.out.println("Tiene que ser un numero el input");
+            System.exit(0);
+        }
 
-        return latin;
+        return 0;
+    }
+
+    private Scanner scanner(){
+        return new Scanner(System.in);
     }
 }
