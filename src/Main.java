e010759b557d51ba2dbe41f688ea9077f53438e1
gonzalo.guasch <gonzaloguasch@outlook.com>
2020-06-08 15:27:50 -0300

    timer del programa

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,17 @@ import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         Input input = new Input();
 
+        long startTime = System.nanoTime();
         Buffer buffer = new Buffer(input.tamBuffer());
         ThreadPool threadPool = new ThreadPool(buffer, input.amountOfLatinWorkers());
 
         threadPool.launch();
+        long endTime = System.nanoTime();
+
+        Thread.sleep(300);
+        System.out.println(((endTime - startTime) / 1000000000));
     }
 }
